3a82817e53820925be45138b8c08626c8ffef1d6
Crowdbotics <team@crowdbotics.com>
2024-08-28 01:14:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "singtel-ai-accelera-49179",
+  url: "https://singtel-ai-accelera-49179.botics.co",
+  api: "https://singtel-ai-accelera-49179.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
